ea0c049d51f79d4626504eb282897572f76c8ebd
DylanFHJoyce <130550024+DylanFHJoyce@users.noreply.github.com>
2024-04-15 19:43:17 +0100

    Update runBERTopic.py

--- runBERTopic.py
+++ runBERTopic.py
@@ -2,17 +2,28 @@ print("MADE IT TO runBERTopic")
 
 from bertopic import BERTopic
 import pickle
-
+import numpy as np
 
 #take general embeddings, parameters
-bertopicModel = BERTopic()
-#fit bertopic model to embeddings
+
 
 with open("genDatasetProcessed.pkl", "rb") as f:
   generalDataset = pickle.load(f)
 
 with open("thisModelGeneralEmbeddings.pkl", "rb") as f:
   generalEmbeddings = pickle.load(f)
+
+
+
+
+#ALSO LOAD THE EMBEDDING MODEL #DONT NEED TO BECAUSE EMBEDDINGS ARE PRECOMPUTED?
+generalEmbeddings = np.load("thisModelGeneralEmbeddings.pkl")
+
+
+#and provide params here
+bertopicModel = BERTopic(docs=generalDataset, embeddings=generalEmbeddings)
+#fit bertopic model to embeddings
+
 
 print("gendatasetLen: ", len(generalDataset), " generalEmbeddings len: ", len(generalEmbeddings))
 
