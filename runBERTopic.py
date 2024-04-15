0c5918ae6e305c350ca2a82b6d098a9140b04080
DylanFHJoyce <130550024+DylanFHJoyce@users.noreply.github.com>
2024-04-15 20:07:14 +0100

    Update runBERTopic.py

--- runBERTopic.py
+++ runBERTopic.py
@@ -4,6 +4,50 @@ from bertopic import BERTopic
 import pickle
 import numpy as np
 
+
+
+
+import gensim
+from gensim.models.ldamodel import LdaModel
+from gensim.corpora import Dictionary
+from gensim.models.coherencemodel import CoherenceModel
+import nltk
+from nltk.tokenize import word_tokenize
+nltk.download('punkt')
+
+print("first need to calculate corpus")
+def NPMICoherence(topicModel, tokenized_corpus, corpusDictionary):
+    
+    # Calculate NPMI coherence
+    coherence_model_npmi = gensim.models.CoherenceModel(topics=topicModel.get_topic_info()["Representation"].tolist(), texts=tokenized_corpus, dictionary=corpusDictionary, coherence='c_npmi')
+    
+    coherence_npmi = coherence_model_npmi.get_coherence()
+    
+    coherence_npmiPerTopic = coherence_model_npmi.get_coherence_per_topic()
+
+    print("NPMI Coherence Score:", coherence_npmi)
+
+    return coherence_npmi, coherence_npmiPerTopic
+    
+def topicDiversity(topicsNWordsList):
+    
+    uniqueSet = set()
+    for topicNWords in topicsNWordsList:
+        
+        thisTopicSet = set(topicNWords)
+        uniqueSet = uniqueSet.union(thisTopicSet)
+    
+    return len(uniqueSet) / (10 *len(topicsNWordsList))
+
+
+
+
+
+
+
+
+
+
 #take general embeddings, parameters
 
 
