8987d6db1f2c8ac8b6b0df6b4ce58e558d8a06e2
DylanFHJoyce <130550024+DylanFHJoyce@users.noreply.github.com>
2024-04-16 12:16:12 +0100

    Update runBERTopic.py

--- runBERTopic.py
+++ runBERTopic.py
@@ -225,7 +225,9 @@ bertResults = pd.DataFrame(columns=["iteration", "TD", "Coherence"])
 bertResults.to_csv("bertResults.csv", index=False)
 
 bertResults = pd.read_csv("bertResults.csv")
-for iteration in range(0, 2):
+
+
+for iteration in range(0, 1):
     
     # ctfidf_model = ClassTfidfTransformer(reduce_frequent_words=True)
     # bertopicModel = BERTopic(min_topic_size=140, ctfidf_model=ctfidf_model)
