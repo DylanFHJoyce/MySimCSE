c142fd84b86a6e1d11743a9d8241ddd3397c3421
DylanFHJoyce <130550024+DylanFHJoyce@users.noreply.github.com>
2024-04-16 09:49:33 +0100

    Update runBERTopic.py

--- runBERTopic.py
+++ runBERTopic.py
@@ -15,6 +15,14 @@ from gensim.models.coherencemodel import CoherenceModel
 import nltk
 from nltk.tokenize import word_tokenize
 
+
+
+#EXPERIMENT 2 IMPORTS
+
+
+
+
+
 #nltk.download('punkt')
 
 print("first need to calculate corpus")
@@ -41,13 +49,129 @@ def topicDiversity(topicsNWordsList): #you give this bertopicmodel.get_topic_inf
     
     return len(uniqueSet) / (10 *len(topicsNWordsList))
     
+def convertTopicNumToName(numList, df):
+    """"
+    takes:
+    list of topic number predictions
+    df from bertModel.get_topic_info() for the bert model that made the predictions
+
+    returns topic (by name not number) for each list entry
+
+    """
+    topicDict = dict(zip(df['Topic'], df['Name']))
+    return [topicDict.get(topic_number, 'Unknown') for topic_number in numList]
+
+def getTopicPredByName(bertModel, trainDocData, docDataEmbedded):
+    """"
+    takes:
+    already fitted bertopic model,
+    df of documents with at least "Document" column, for which we want to see predictions,
+    embeddings for the documents
+
+    #transforms documents to the model and retrieves topic number predictions
+    #converts topic number predictions to topic name predictions
+
+    returns labelled training df with topic name predicitons attatched
+
+    """
     
+    trainDocData = trainDocData.reset_index(drop=True)
+    trainDocs = trainDocData["Document"]
+    if type(docDataEmbedded) == torch.Tensor:
+        docDataEmbedded = docDataEmbedded.numpy()
+
+    print("tempCHange in get top pred name")
+    #bertModel.transform(trainDocs, docDataEmbedded) #
+    pred, prob = bertModel.transform(trainDocData, docDataEmbedded)
+
+    topicNamePredictions = convertTopicNumToName(pred, bertModel.get_topic_info())
+
+    #print(len(topicNamePredictions), len(trainDocData))
+
+    trainDocData["BertPredictions"] = topicNamePredictions
+
+    return trainDocData
+
+def compareTrainTopicsToBTopics(berTopicModel, trainingDataDF, trainingDataEmbeddings):
+    '''
+    fitted bert model
+    training data df with columns for document and category
+    embeddings for those documents
+
+    returns crosstab (freq table) for how many times samples from each theme ended up in each topic
+    
+    '''
+    TPbyName = getTopicPredByName(berTopicModel, trainingDataDF, trainingDataEmbeddings)
+    
+    return pd.crosstab(TPbyName["Category"], TPbyName["BertPredictions"])
+
+def statsFromCrosstab(crosstab):
+    #print("for predictions frequency if two catas have high perc in same BT T analyse")
+
+    #for each training category what bertopic topic did its samples end up in most
+    most_common_predictions = crosstab.idxmax(axis=1)
+
+    # for each trainging Category Calculate the percentage that ended up in each BertPrediction 
+    #THIS IS X% (as 0.X) of label cata ended up in B topic
+    prediction_frequency = crosstab.div(crosstab.sum(axis=1), axis=0)
+    #and this is the inverse: X% of this B topic came from this label cata
+    BTTrainComp = BTopicPercentageTrainComposition(crosstab)
+
+    # Calculate the total count of samples for each BertPrediction
+    total_samples_per_prediction = crosstab.sum(axis=0)
+    # Calculate the composition of each BertPrediction in terms of Category
+    prediction_composition = crosstab.div(total_samples_per_prediction, axis=1)
+
+    #find the categorys that are most spread out across BertPredictions
+    category_spread = prediction_frequency.std(axis=1)
+    average_category_spread = category_spread.mean()
+
+    #find the least spread Categorys
+    least_spread_categories = category_spread.nsmallest(5)  # Adjust the number as needed
+
+    # find the MOST spread Categorys
+    most_spread_categories = category_spread.nlargest(5)  # Adjust the number as needed
+
+    return most_common_predictions, prediction_frequency, total_samples_per_prediction, prediction_composition, average_category_spread, category_spread, least_spread_categories, most_spread_categories, BTTrainComp
+
+def BTopicPercentageTrainComposition(crosstab):
+    '''
+    X% of this B topic came from this label cata
+    '''
+    newDF = pd.DataFrame(index = crosstab.index)
+    for BertTopicGenTopic in crosstab.columns: # for each bertopic topic
+        totalSamples = crosstab[BertTopicGenTopic].sum()
+        relevance_percentage = (crosstab[BertTopicGenTopic] / totalSamples) * 100
+        #print(relevance_percentage)
+        newDF[BertTopicGenTopic] = relevance_percentage
+    return newDF
+
+def topicsToThemes(BTTrainComp):
+    ''' 
+    
+    give BTTrainComp from statsFromCrosstab
+    
+    #unseenGeneralDatasetDocs for merge
+
+    converts bert gen topics to themes based on training (or held out) data content
+    '''
+
+    #for each bertopic topic generate empty dict entry
+    bertTopicCorrelateDict = {key: [] for key in BTTrainComp.columns.values.tolist()}
+    
+    #assign each topic to the theme that it contains the most samples from (held out data)
+    for bertGenTopic in BTTrainComp.columns:
+        topicBelongsToTheme = BTTrainComp[bertGenTopic].idxmax()
+        bertTopicCorrelateDict.update({bertGenTopic: topicBelongsToTheme})
+        
+    #for bertTopicCorrelateDict where value is still empty
+    #find non empty/most similar topic on same level of hierarchy / adjacent / above?
+    #merge it
+
+    print("but if the theme contains no samples... for all dict entries that are still empty: start at lowest hierarchy layer?")
 
 
-
-
-
-
+    return bertTopicCorrelateDict
 
 
 #take general embeddings, parameters
@@ -122,6 +246,14 @@ for iteration in range(0, 2):
     coherenceTuple = NPMICoherence(bertopicModel, corpusAndDictionaryLabelInc[0], corpusAndDictionaryLabelInc[1])
     print("Coherence: ", coherenceTuple)
     ################
+
+
+    #crosstab = compareTrainTopicsToBTopics(bertopicModel, HELD OUT TRAINING DATA LIST(CHECK IF NEEDS TO BE DF), HELD OUT training data embeddings) 
+    print("Many crosstab stats may be unnecessary in the final version")
+    # statsFromCT = statsFromCrosstab(crosstab)
+    print("Many crosstab stats may be unnecessary in the final version, but it might not matter")
+    #most_common_predictions, prediction_frequency, total_samples_per_prediction, prediction_composition, average_category_spread, category_spread, least_spread_categories, most_spread_categories, BTTrainComp = statsFromCT
+    #topicsToThemesDict = topicsToThemes(BTTrainComp)
     
     print(bertopicModel.get_topic_info()["Representation"].tolist())
     print(len(bertopicModel.get_topic_info()["Representation"].tolist()))
