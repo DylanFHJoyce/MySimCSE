23a8b91b8619adb1a8619a007455ec7db0149616
DylanFHJoyce <130550024+DylanFHJoyce@users.noreply.github.com>
2024-04-20 10:21:59 +0100

    Create ThemeFocusedTraining.py

---
+++ ThemeFocusedTraining.py
@@ -0,0 +1,15 @@
+
+
+
+#use either base model or sim model to start
+
+#do embeddings
+
+#do bert model and use theme spread analysis to decide upon themes to train
+
+
+#turn labelled training data into triplet dataset based on theme (keep small percentage of general data to keep context)
+
+#run training 
+
+#do bertopic model and spread analysis and compare to starting one 
