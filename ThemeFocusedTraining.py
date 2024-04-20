030167d28ac8508a0188beceeedd5e628c4d5d6e
DylanFHJoyce <130550024+DylanFHJoyce@users.noreply.github.com>
2024-04-20 23:48:15 +0100

    Update ThemeFocusedTraining.py

--- ThemeFocusedTraining.py
+++ ThemeFocusedTraining.py
@@ -181,7 +181,7 @@ trainingTripletsCSV = "specificThemeTripletDataset.csv"
 learning_rate =5e-6
 per_device_train_batch_size = 64 #CHANGE THIS IF USING LOWER QUANTITIES OF TRAINING DATA OR DUPLICATE TRAINING DATA
 
-
+print("firstTrain")
 for ThemeFocusedIteration in range(0, 3):
     runSim(startingModel, trainingTripletsCSV, learning_rate, 3, output_dir, per_device_train_batch_size)
     startingModel = output_dir #after first training run we use that model for each subsequent run
@@ -217,7 +217,7 @@ for ThemeFocusedIteration in range(0, 3):
     # #do bert model and use theme spread analysis to decide upon themes to train
     
     
-    print("STARTING SECOND THEME SPREAD ANALYSIS")
+    print("\n\nSTARTING SECOND THEME SPREAD ANALYSIS/ secondTrain")
     runThemeSpreadAnalysis()
 
 
