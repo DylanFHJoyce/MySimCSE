9428d8cc6ad2cb9dc55dc626c2e1d4a67b988858
DylanFHJoyce <130550024+DylanFHJoyce@users.noreply.github.com>
2024-04-20 11:43:33 +0100

    Update runThemeSpreadAnalysis.py

--- runThemeSpreadAnalysis.py
+++ runThemeSpreadAnalysis.py
@@ -1,4 +1,18 @@
 print("THEME SPREAD TEST")
+from bertopic import BERTopic
+from bertopic.vectorizers import ClassTfidfTransformer
+import pickle
+import numpy as np
+import pandas as pd
+import os
+
+import gensim
+from gensim.models.ldamodel import LdaModel
+from gensim.corpora import Dictionary
+from gensim.models.coherencemodel import CoherenceModel
+import nltk
+from nltk.tokenize import word_tokenize
+
 
 
 #do bert model on gen dataset
