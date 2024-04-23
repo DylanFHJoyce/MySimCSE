462ad928052a09519d522e235652d92f8502b1f9
DylanFHJoyce <130550024+DylanFHJoyce@users.noreply.github.com>
2024-04-23 22:29:04 +0100

    Update runThemeSpreadAnalysis.py

--- runThemeSpreadAnalysis.py
+++ runThemeSpreadAnalysis.py
@@ -234,12 +234,12 @@ coherenceTuple = (0, 0)
 
 #start loop here
 
-topicSizes = [20, 50, 80]
+topicSizes = [20, 40, 60, 80, 100]
 topicSizes = [40]
 #
 print("MIN TOPIC SIZE CHANGED TO NR_TOPICS")
 for min_topic_size in topicSizes:
-    for iteration in range(0, 1):
+    for iteration in range(0, 2):
         
         bertopicModel = BERTopic(min_topic_size=min_topic_size)
         bertopicModel.fit(documents=generalDataset, embeddings=ThemeSpreadEmbeddings)
