3b46b1067682964939eae7cba1e12a79c2e2b16e
DylanFHJoyce <130550024+DylanFHJoyce@users.noreply.github.com>
2024-04-23 23:43:37 +0100

    Update SplitThemesCondencedTopic.py

--- SplitThemesCondencedTopic.py
+++ SplitThemesCondencedTopic.py
@@ -1,4 +1,77 @@
-print("test print")
+import subprocess
+from simcse import SimCSE
+import pickle
+import numpy as np
+import pandas as pd
+
+import random
+
+
+def runThemeSpreadAnalysis():
+    #DOES THIS NEED SHELL = TRUE ASWELL?
+    command = "conda run -n berTopicEnv python runThemeSpreadAnalysis.py"
+
+    #test bert will take general embeddings, parameters
+
+    #fit bertopic model to embeddings
+
+    #evaluate and store evaluation
+
+    subprocess.run(command, shell=True)
+
+def generate_triplet_dataset(input_df, length):
+    # lists for triplet data
+    sent0_list, sent1_list, hard_neg_list = [], [], []
+
+    for _ in range(length):
+        # Randomly select a row from the input DataFrame
+        random_row_index = random.randint(0, len(input_df) - 1)
+        sent0_row = input_df.iloc[random_row_index]
+
+        # Randomly select a row from the same category as sent0
+        same_category_rows = input_df[input_df['Category'] == sent0_row['Category']]
+        sent1_row = same_category_rows.sample(1).iloc[0]
+
+        # Randomly select a row from a different category than sent0
+        different_category_rows = input_df[input_df['Category'] != sent0_row['Category']]
+        hard_neg_row = different_category_rows.sample(1).iloc[0]
+
+        # Append the selected rows to the lists
+        sent0_list.append(sent0_row['Document'])
+        sent1_list.append(sent1_row['Document'])
+        hard_neg_list.append(hard_neg_row['Document'])
+
+    # Create the triplet DataFrame
+    triplet_df = pd.DataFrame({
+        'sent0': sent0_list,
+        'sent1': sent1_list,
+        'hard_neg': hard_neg_list
+    })
+
+    return triplet_df
+
+
+def runSim(startingModel, trainingTripletsCSV, learning_rate, num_epochs, output_dir, per_device_train_batch_size):
+  command = (
+    "conda run -n simEnv python train.py "
+    f"--model_name_or_path {startingModel} "
+    f"--train_file {trainingTripletsCSV} "
+    f"--output_dir {output_dir} "
+    f"--num_train_epochs {num_epochs} "
+    f"--per_device_train_batch_size {per_device_train_batch_size} "
+    f"--learning_rate {learning_rate} "
+    "--max_seq_length 64 "
+    "--load_best_model_at_end "
+    "--pooler_type cls "
+    "--overwrite_output_dir "
+    "--temp 0.05 "
+     "--do_train "
+     "--fp16 "
+     "--use_in_batch_instances_as_negatives"
+  )
+  subprocess.run(command, shell=True)
+
+
 
 #have the sections all in here and comment out at whatever stage in the process, its messy but i need to save time.
 
