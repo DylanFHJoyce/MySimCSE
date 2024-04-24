6be3583a1b7318598785d61d9f718d2e200039db
DylanFHJoyce <130550024+DylanFHJoyce@users.noreply.github.com>
2024-04-24 17:54:09 +0100

    Update runThemeSpreadAnalysis.py

--- runThemeSpreadAnalysis.py
+++ runThemeSpreadAnalysis.py
@@ -255,7 +255,7 @@ coherenceTuple = (0, 0)
 #start loop here
 
 topicSizes = [20, 50, 70, 100]
-#topicSizes = [40]
+topicSizes = [40]
 #
 print("MIN TOPIC SIZE CHANGED TO NR_TOPICS")
 for min_topic_size in topicSizes:
