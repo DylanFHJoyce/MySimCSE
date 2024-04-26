da29f7ed277a178fe9851a59c77cc3364b64d89b
DylanFHJoyce <130550024+DylanFHJoyce@users.noreply.github.com>
2024-04-26 11:55:19 +0100

    Update ThemeFocusedTrainingV2.py

--- ThemeFocusedTrainingV2.py
+++ ThemeFocusedTrainingV2.py
@@ -260,7 +260,7 @@ for learning_rate in learning_rates: #for x in range(0, 11, 2):
         print("LR STARTING: ", learning_rate)
         #output_dir = "bertout"
         #runSim(startingModel, trainingTripletsCSV, learning_rate, 6, output_dir, per_device_train_batch_size)
-        runSim(startingModel, trainingTripletsCSV, learning_rate, ThemeFocusedIteration, output_dir, per_device_train_batch_size)
+        runSim(startingModel, trainingTripletsCSV, learning_rate, 4, output_dir, per_device_train_batch_size)
        
         #startingModel = output_dir #after first training run we use that model for each subsequent run
         #trainingTripletsCSV = "SECONDspecificThemeTripletDataset"
