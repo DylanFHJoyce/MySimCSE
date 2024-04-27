7375e3972f8e64460c119f0c1fca39ecdb7bcf21
DylanFHJoyce <130550024+DylanFHJoyce@users.noreply.github.com>
2024-04-27 20:09:35 +0100

    Update ThemeFocusedTrainingV2.py

--- ThemeFocusedTrainingV2.py
+++ ThemeFocusedTrainingV2.py
@@ -329,7 +329,7 @@ trainingTripletsCSV = "concThemeTriplets.csv" #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
 learning_rates = [2.5e-5]#5e-6]#5e-5]#2.5e-5]#[1.5e-4, 3e-4]#2.5e-5, 7.5e-5]#5e-5, 5e-6] #0, 1e-4, done already
-per_device_train_batch_size = 64 #CHANGE THIS IF USING LOWER QUANTITIES OF TRAINING DATA OR DUPLICATE TRAINING DATA
+per_device_train_batch_size = 32 #CHANGE THIS IF USING LOWER QUANTITIES OF TRAINING DATA OR DUPLICATE TRAINING DATA
 
 print("firstTrain")
 for learning_rate in learning_rates: #for x in range(0, 11, 2):
@@ -352,7 +352,7 @@ for learning_rate in learning_rates: #for x in range(0, 11, 2):
         #runSim(startingModel, trainingTripletsCSV, learning_rate, 6, output_dir, per_device_train_batch_size)
 
         print("\nStarting Model: ", startingModel, "\ntrainingTripletsCSV: ", trainingTripletsCSV, "\n\n")
-        runSim(startingModel, trainingTripletsCSV, learning_rate, 4, output_dir, per_device_train_batch_size)
+        runSim(startingModel, trainingTripletsCSV, learning_rate, ThemeFocusedIteration, output_dir, per_device_train_batch_size)
         #runSim(startingModel, trainingTripletsCSV, learning_rate, 4, output_dir, per_device_train_batch_size)
 
 
